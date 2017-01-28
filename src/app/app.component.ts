574764db53938f22488bebe79adc973c880e1a3f
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2017-01-28 13:22:57 +0000

    Imported Poseidon NG

---
+++ src/app/app.component.ts
@@ -0,0 +1,202 @@
+import {Component,AfterViewInit,ElementRef,Renderer,ViewChild} from '@angular/core';
+
+enum MenuOrientation {
+    STATIC,
+    OVERLAY,
+    HORIZONTAL
+};
+
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit {
+    
+    layoutCompact: boolean = false;
+
+    layoutMode: MenuOrientation = MenuOrientation.STATIC;
+    
+    darkMenu: boolean = false;
+    
+    profileMode: string = 'inline';
+
+    rotateMenuButton: boolean;
+
+    topbarMenuActive: boolean;
+
+    overlayMenuActive: boolean;
+
+    staticMenuDesktopInactive: boolean;
+
+    staticMenuMobileActive: boolean;
+
+    layoutContainer: HTMLDivElement;
+
+    layoutMenuScroller: HTMLDivElement;
+
+    modal: HTMLDivElement;
+
+    menuClick: boolean;
+
+    topbarItemClick: boolean;
+
+    activeTopbarItem: any;
+
+    documentClickListener: Function;
+
+    resetMenu: boolean;
+
+    @ViewChild('layoutWrapper') layourContainerViewChild: ElementRef;
+
+    @ViewChild('layoutMenuScroller') layoutMenuScrollerViewChild: ElementRef;
+
+    constructor(public renderer: Renderer) {}
+
+    ngAfterViewInit() {
+        this.layoutContainer = <HTMLDivElement> this.layourContainerViewChild.nativeElement;
+        this.layoutMenuScroller = <HTMLDivElement> this.layoutMenuScrollerViewChild.nativeElement;
+
+        //hides the horizontal submenus or top menu if outside is clicked
+        this.documentClickListener = this.renderer.listenGlobal('body', 'click', (event) => {
+            if(!this.topbarItemClick) {
+                this.activeTopbarItem = null;
+                this.topbarMenuActive = false;
+            }
+
+            if(!this.menuClick) {
+                this.resetMenu = true;
+            }
+
+            this.topbarItemClick = false;
+            this.menuClick = false;
+        });
+        
+        setTimeout(() => {
+            jQuery(this.layoutMenuScroller).nanoScroller({flash:true});
+        }, 10);
+    }
+
+    onMenuButtonClick(event) {
+        this.rotateMenuButton = !this.rotateMenuButton;
+        this.topbarMenuActive = false;
+
+        if(this.layoutMode === MenuOrientation.OVERLAY) {
+            this.overlayMenuActive = !this.overlayMenuActive;
+
+            if(this.overlayMenuActive)
+                this.enableModal();
+            else
+                this.disableModal();
+        }
+        else {
+            if(this.isDesktop()) {
+                this.staticMenuDesktopInactive = !this.staticMenuDesktopInactive;
+            }
+            else {
+                if(this.staticMenuMobileActive) {
+                    this.staticMenuMobileActive = false;
+                    this.disableModal();
+                }
+                else {
+                    this.staticMenuMobileActive = true;
+                    this.enableModal();
+                }
+            }
+        }
+
+        event.preventDefault();
+    }
+
+    onMenuClick($event) {
+        this.menuClick = true;
+        this.resetMenu = false;
+
+        if(!this.isHorizontal()) {
+            setTimeout(() => {
+                jQuery(this.layoutMenuScroller).nanoScroller();
+            }, 500);
+        }
+    }
+
+    onTopbarMenuButtonClick(event) {
+        this.topbarItemClick = true;
+        this.topbarMenuActive = !this.topbarMenuActive;
+        
+        if(this.overlayMenuActive || this.staticMenuMobileActive) {
+            this.rotateMenuButton = false;
+            this.overlayMenuActive = false;
+            this.staticMenuMobileActive = false;
+            this.disableModal();
+        }
+    }
+
+    onTopbarItemClick(event, item) {
+        this.topbarItemClick = true;
+
+        if(this.activeTopbarItem === item)
+            this.activeTopbarItem = null;
+        else
+            this.activeTopbarItem = item;
+
+        event.preventDefault();
+    }
+
+    enableModal() {
+        this.modal = document.createElement("div");
+        this.modal.className = 'layout-mask';
+        this.layoutContainer.appendChild(this.modal);
+    }
+    
+    disableModal() {
+        if(this.modal) {
+            this.layoutContainer.removeChild(this.modal);
+        }
+    }
+
+    isTablet() {
+        let width = window.innerWidth;
+        return width <= 1024 && width > 640;
+    }
+
+    isDesktop() {
+        return window.innerWidth > 1024;
+    }
+
+    isMobile() {
+        return window.innerWidth <= 640;
+    }
+
+    isOverlay() {
+        return this.layoutMode === MenuOrientation.OVERLAY;
+    }
+
+    isHorizontal() {
+        return this.layoutMode === MenuOrientation.HORIZONTAL;
+    }
+
+    changeToStaticMenu() {
+        this.layoutMode = MenuOrientation.STATIC;
+    }
+
+    changeToOverlayMenu() {
+        this.layoutMode = MenuOrientation.OVERLAY;
+    }
+
+    changeToHorizontalMenu() {
+        this.layoutMode = MenuOrientation.HORIZONTAL;
+    }
+
+    ngOnDestroy() {
+        this.disableModal();
+
+        if(this.documentClickListener) {
+            this.documentClickListener();
+        }  
+
+        jQuery(this.layoutMenuScroller).nanoScroller({flash:true});
+    }
+
+}
---
+++ src/app/app.menu.component.ts
@@ -0,0 +1,208 @@
+import {Component,Input,OnInit,EventEmitter,ViewChild,trigger,state,transition,style,animate,Inject,forwardRef} from '@angular/core';
+import {Location} from '@angular/common';
+import {Router} from '@angular/router';
+import {MenuItem} from 'primeng/primeng';
+import {AppComponent} from './app.component';
+
+@Component({
+    selector: 'app-menu',
+    template: `
+        <ul app-submenu [item]="model" root="true" class="layout-menu clearfix" [reset]="reset"></ul>
+    `
+})
+export class AppMenuComponent implements OnInit {
+
+    @Input() reset: boolean;
+
+    model: MenuItem[];
+
+    constructor(@Inject(forwardRef(() => AppComponent)) public app:AppComponent) {}
+    
+    ngOnInit() {
+        this.model = [
+            {label: 'Dashboard', icon: 'fa fa-fw fa-home', routerLink: ['/']},
+            {
+                label: 'Themes', icon: 'fa fa-fw fa-paint-brush',
+                items: [
+                    {label: 'Indigo - Pink', icon: 'fa fa-fw fa-paint-brush', command: (event) => {this.changeTheme('indigo')}}
+                ]
+            },
+            {
+                label: 'Customization', icon: 'fa fa-fw fa-bars',
+                items: [
+                    {label: 'Static Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToStaticMenu()},
+                    {label: 'Overlay Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToOverlayMenu()},
+                    {label: 'Horizontal Menu', icon: 'fa fa-fw fa-bars',  command: () => this.app.changeToHorizontalMenu()}
+                ]
+            },
+            {
+                label: 'Components', icon: 'fa fa-fw fa-sitemap',
+                items: [
+                    {label: 'Sample Page', icon: 'fa fa-fw fa-columns', routerLink: ['/sample']},
+                    {label: 'Forms', icon: 'fa fa-fw fa-code', routerLink: ['/forms']},
+                    {label: 'Data', icon: 'fa fa-fw fa-table', routerLink: ['/data']},
+                    {label: 'Panels', icon: 'fa fa-fw fa-list-alt', routerLink: ['/panels']},
+                    {label: 'Overlays', icon: 'fa fa-fw fa-square', routerLink: ['/overlays']},
+                    {label: 'Menus', icon: 'fa fa-fw fa-minus-square-o', routerLink: ['/menus']},
+                    {label: 'Messages', icon: 'fa fa-fw fa-circle-o-notch', routerLink: ['/messages']},
+                    {label: 'Charts', icon: 'fa fa-fw fa-area-chart', routerLink: ['/charts']},
+                    {label: 'File', icon: 'fa fa-fw fa-arrow-circle-o-up', routerLink: ['/file']},
+                    {label: 'Misc', icon: 'fa fa-fw fa-user-secret', routerLink: ['/misc']}
+                ]
+            },
+            {
+                label: 'Template Pages', icon: 'fa fa-fw fa-life-saver',
+                items: [
+                    {label: 'Empty Page', icon: 'fa fa-fw fa-square-o', routerLink: ['/empty']},
+                    {label: 'Landing Page', icon: 'fa fa-fw fa-certificate', url: 'landing.html'},
+                    {label: 'Login Page', icon: 'fa fa-fw fa-sign-in', url: 'login.html'},
+                    {label: 'Error Page', icon: 'fa fa-fw fa-exclamation-circle', url: 'error.html'},
+                    {label: '404 Page', icon: 'fa fa-fw fa-times', url: '404.html'},
+                    {label: 'Access Denied Page', icon: 'fa fa-fw fa-exclamation-triangle', url: 'access.html'}
+                ]
+            },
+            {
+                label: 'Menu Hierarchy', icon: 'fa fa-fw fa-gg',
+                items: [
+                    {
+                        label: 'Submenu 1', icon: 'fa fa-fw fa-sign-in',
+                        items: [
+                            {
+                                label: 'Submenu 1.1', icon: 'fa fa-fw fa-sign-in',
+                                items: [
+                                    {label: 'Submenu 1.1.1', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 1.1.2', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 1.1.3', icon: 'fa fa-fw fa-sign-in'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 1.2', icon: 'fa fa-fw fa-sign-in',
+                                items: [
+                                    {label: 'Submenu 1.2.1', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 1.2.2', icon: 'fa fa-fw fa-sign-in'}
+                                ]
+                            },
+                        ]
+                    },
+                    {
+                        label: 'Submenu 2', icon: 'fa fa-fw fa-sign-in',
+                        items: [
+                            {
+                                label: 'Submenu 2.1', icon: 'fa fa-fw fa-sign-in',
+                                items: [
+                                    {label: 'Submenu 2.1.1', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 2.1.2', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 2.1.3', icon: 'fa fa-fw fa-sign-in'},
+                                ]
+                            },
+                            {
+                                label: 'Submenu 2.2', icon: 'fa fa-fw fa-sign-in',
+                                items: [
+                                    {label: 'Submenu 2.2.1', icon: 'fa fa-fw fa-sign-in'},
+                                    {label: 'Submenu 2.2.2', icon: 'fa fa-fw fa-sign-in'}
+                                ]
+                            },
+                        ]
+                    }
+                ]
+            },
+            {label: 'Documentation', icon: 'fa fa-fw fa-book', routerLink: ['/documentation']}
+        ];
+    }
+
+    changeTheme(theme) {
+        let themeLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('theme-css');
+        let layoutLink: HTMLLinkElement = <HTMLLinkElement> document.getElementById('layout-css');
+        
+        themeLink.href = 'assets/theme/theme-' + theme +'.css';
+        layoutLink.href = 'assets/layout/css/layout-' + theme +'.css';
+    }
+}
+
+@Component({
+    selector: '[app-submenu]',
+    template: `
+        <template ngFor let-child let-i="index" [ngForOf]="(root ? item : item.items)">
+            <li [ngClass]="{'active-menuitem': isActive(i)}">
+                <a [href]="child.url||'#'" (click)="itemClick($event,child,i)" *ngIf="!child.routerLink">
+                    <i [ngClass]="child.icon"></i>
+                    <span>{{child.label}}</span>
+                    <i class="fa fa-fw fa-angle-down" *ngIf="child.items"></i>
+                </a>
+
+                <a (click)="itemClick($event,child,i)" class="ripplelink" *ngIf="child.routerLink"
+                    [routerLink]="child.routerLink" routerLinkActive="active-menuitem-routerlink" [routerLinkActiveOptions]="{exact: true}">
+                    <i [ngClass]="child.icon"></i>
+                    <span>{{child.label}}</span>
+                    <i class="fa fa-fw fa-angle-down" *ngIf="child.items"></i>
+                </a>
+                <ul app-submenu [item]="child" *ngIf="child.items" [@children]="isActive(i) ? 'visible' : 'hidden'"></ul>
+            </li>
+        </template>
+    `,
+    animations: [
+        trigger('children', [
+            state('hidden', style({
+                height: '0px'
+            })),
+            state('visible', style({
+                height: '*'
+            })),
+            transition('visible => hidden', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
+            transition('hidden => visible', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
+        ])
+    ]
+})
+export class AppSubMenu {
+
+    @Input() item: MenuItem;
+    
+    @Input() root: boolean;
+
+    _reset: boolean;
+        
+    activeIndex: number;
+
+    constructor(@Inject(forwardRef(() => AppComponent)) public app:AppComponent, public router: Router, public location: Location) {}
+        
+    itemClick(event: Event, item: MenuItem, index: number) {
+        if(item.disabled) {
+            event.preventDefault();
+            return true;
+        }
+        
+        this.activeIndex = (this.activeIndex === index) ? null : index;
+                
+        if(item.command) {
+            if(!item.eventEmitter) {
+                item.eventEmitter = new EventEmitter();
+                item.eventEmitter.subscribe(item.command);
+            }
+            
+            item.eventEmitter.emit({
+                originalEvent: event,
+                item: item
+            });
+        }
+
+        if(item.items || !item.url) {
+            event.preventDefault();
+        }
+    }
+    
+    isActive(index: number): boolean {
+        return this.activeIndex === index;
+    }
+
+    @Input() get reset(): boolean {
+        return this._reset;
+    }
+
+    set reset(val:boolean) {
+        this._reset = val;
+
+        if(this._reset && this.app.isHorizontal()) {
+            this.activeIndex = null;
+        }
+    }
+}
