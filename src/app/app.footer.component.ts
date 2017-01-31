645bf708cd2a0ce41047b64b4cc8b2203055ddc5
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2017-01-31 07:43:59 +0000

    fixes

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -6,7 +6,7 @@ import {AppComponent} from './app.component';
     template: `
         <div class="footer">
             <div class="card clearfix">
-                <span class="footer-text-left">PrimeNG ULTIMA for Angular 2</span>
+                <span class="footer-text-left">PrimeNG POSEIDON for Angular</span>
                 <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
             </div>
         </div>
