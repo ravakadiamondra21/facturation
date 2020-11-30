158c0111b69af7292e50f33a227de737d002b7d7
Merve Özçifçi <merveozcifci7@gmail.com>
2020-11-30 22:16:39 +0300

    invoice logo fix

--- src/app/pages/app.invoice.component.ts
+++ src/app/pages/app.invoice.component.ts
@@ -1,16 +1,27 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AppMainComponent } from '../app.main.component';
 import {AppBreadcrumbService} from '../app.breadcrumb.service';
 
 @Component({
     templateUrl: './app.invoice.component.html'
 })
-export class AppInvoiceComponent {
+export class AppInvoiceComponent implements OnInit{
 
-    constructor(private breadcrumbService: AppBreadcrumbService) {
+    constructor(private breadcrumbService: AppBreadcrumbService, public app: AppMainComponent) {
         this.breadcrumbService.setItems([
             { label: 'Pages' },
             { label: 'Invoice', routerLink: ['/pages/invoice'] }
         ]);
+    }
+
+    ngOnInit(): void {
+        const invoiceLogoLink: HTMLImageElement = document.getElementById('invoice-logo') as HTMLImageElement;
+
+        const logoUrl = `assets/layout/images/logo-${this.app.layoutMode === 'light' ? 'poseidon' : 'poseidon-dark'}.png`;
+
+        if (invoiceLogoLink) {
+            invoiceLogoLink.src = logoUrl;
+        }
     }
 
     print() {
